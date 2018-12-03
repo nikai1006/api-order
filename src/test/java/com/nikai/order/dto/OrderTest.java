c9c87076506258c24583b8b8563d154ca5713a09
keni <keni@aijiatui.com>
2018-12-03 10:02:31 +0800

    test

---
+++ src/test/java/com/nikai/order/dto/OrderTest.java
@@ -0,0 +1,289 @@
+package com.nikai.order.dto;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OrderTest {
+
+    @Test
+    public void getUserId() {
+    }
+
+    @Test
+    public void setUserId() {
+    }
+
+    @Test
+    public void getUserName() {
+    }
+
+    @Test
+    public void setUserName() {
+    }
+
+    @Test
+    public void getOrderAmount() {
+    }
+
+    @Test
+    public void setOrderAmount() {
+    }
+
+    @Test
+    public void getPostage() {
+    }
+
+    @Test
+    public void setPostage() {
+    }
+
+    @Test
+    public void getCouponAmount() {
+    }
+
+    @Test
+    public void setCouponAmount() {
+    }
+
+    @Test
+    public void getCouponCode() {
+    }
+
+    @Test
+    public void setCouponCode() {
+    }
+
+    @Test
+    public void getReduceAmount() {
+    }
+
+    @Test
+    public void setReduceAmount() {
+    }
+
+    @Test
+    public void getPayType() {
+    }
+
+    @Test
+    public void setPayType() {
+    }
+
+    @Test
+    public void getPaymentFlowNo() {
+    }
+
+    @Test
+    public void setPaymentFlowNo() {
+    }
+
+    @Test
+    public void getPayNo() {
+    }
+
+    @Test
+    public void setPayNo() {
+    }
+
+    @Test
+    public void getDeliveryCode() {
+    }
+
+    @Test
+    public void setDeliveryCode() {
+    }
+
+    @Test
+    public void getDeliverCompanyName() {
+    }
+
+    @Test
+    public void setDeliverCompanyName() {
+    }
+
+    @Test
+    public void getPayTime() {
+    }
+
+    @Test
+    public void setPayTime() {
+    }
+
+    @Test
+    public void getCreatedTime() {
+    }
+
+    @Test
+    public void setCreatedTime() {
+    }
+
+    @Test
+    public void getCompletedTime() {
+    }
+
+    @Test
+    public void setCompletedTime() {
+    }
+
+    @Test
+    public void getCloseTime() {
+    }
+
+    @Test
+    public void setCloseTime() {
+    }
+
+    @Test
+    public void getPickingTime() {
+    }
+
+    @Test
+    public void setPickingTime() {
+    }
+
+    @Test
+    public void getDeleteStatus() {
+    }
+
+    @Test
+    public void setDeleteStatus() {
+    }
+
+    @Test
+    public void getFrom() {
+    }
+
+    @Test
+    public void setFrom() {
+    }
+
+    @Test
+    public void getPayFrom() {
+    }
+
+    @Test
+    public void setPayFrom() {
+    }
+
+    @Test
+    public void getUserRemark() {
+    }
+
+    @Test
+    public void setUserRemark() {
+    }
+
+    @Test
+    public void getReason() {
+    }
+
+    @Test
+    public void setReason() {
+    }
+
+    @Test
+    public void getSellerId() {
+    }
+
+    @Test
+    public void setSellerId() {
+    }
+
+    @Test
+    public void getSellerName() {
+    }
+
+    @Test
+    public void setSellerName() {
+    }
+
+    @Test
+    public void getSellerRemark() {
+    }
+
+    @Test
+    public void setSellerRemark() {
+    }
+
+    @Test
+    public void getManageComments() {
+    }
+
+    @Test
+    public void setManageComments() {
+    }
+
+    @Test
+    public void getAmount() {
+    }
+
+    @Test
+    public void setAmount() {
+    }
+
+    @Test
+    public void getVerifyTime() {
+    }
+
+    @Test
+    public void setVerifyTime() {
+    }
+
+    @Test
+    public void getType() {
+    }
+
+    @Test
+    public void setType() {
+    }
+
+    @Test
+    public void getSubType() {
+    }
+
+    @Test
+    public void setSubType() {
+    }
+
+    @Test
+    public void getExpireTime() {
+    }
+
+    @Test
+    public void setExpireTime() {
+    }
+
+    @Test
+    public void getCurrentServerTime() {
+    }
+
+    @Test
+    public void setCurrentServerTime() {
+    }
+
+    @Test
+    public void getGoodsNum() {
+    }
+
+    @Test
+    public void setGoodsNum() {
+    }
+
+    @Test
+    public void getGoods() {
+    }
+
+    @Test
+    public void setGoods() {
+    }
+
+    @Test
+    public void getMultiTypeDto() {
+    }
+
+    @Test
+    public void setMultiTypeDto() {
+    }
+
+}
